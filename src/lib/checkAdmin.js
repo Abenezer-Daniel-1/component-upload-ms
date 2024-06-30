de9d9aff8b29ef83f42ad972f21b0e6ed4acc19a
Abenezer-Daniel-1 <abenezerdanieldesalegn@gmail.com>
2024-06-30 13:17:49 +0300

    admin check test logs

--- src/lib/checkAdmin.js
+++ src/lib/checkAdmin.js
@@ -1,6 +1,12 @@
 module.exports = (req, res, next) => {
-    if(!req.cookies.admin_check || req.cookies.admin_check !== process.env.ADMIN_CHECK) 
+    if(!req.cookies.admin_check || req.cookies.admin_check !== process.env.ADMIN_CHECK) {
+        console.log("Test logs...");
+        console.log(process.env.ADMIN_CHECK);
+        console.log(req.cookies);
+        console.log(req.cookies.admin_check);
+
         return res.send("You are not the admin, what are you doing here?");
+    }
 
     next();
 }
